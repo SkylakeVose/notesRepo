a1fc7058b45af44e4205742416b95d96d1b684f6
skyLake_vose <912214336@qq.com>
2025-11-15 17:43:10 +0800

    update

---
+++ Spring6/code/Spring6/spring6-003-dependency-injection/src/main/java/cn/piggy/spring6/bean/Season.java
@@ -0,0 +1,5 @@
+package cn.piggy.spring6.bean;
+
+public enum Season {
+    SPRING, SUMMER, AUTUMN, WINTER
+}
--- Spring6/code/Spring6/spring6-003-dependency-injection/src/test/java/cn/piggy/spring6/test/SpringDITest.java
+++ Spring6/code/Spring6/spring6-003-dependency-injection/src/test/java/cn/piggy/spring6/test/SpringDITest.java
@@ -1,11 +1,41 @@
 package cn.piggy.spring6.test;
 
+import cn.piggy.spring6.bean.SimpleValueType;
+import cn.piggy.spring6.bean.User;
+import cn.piggy.spring6.jdbc.MyDataSource;
 import cn.piggy.spring6.service.CustomerService;
+import cn.piggy.spring6.service.OrderService;
 import cn.piggy.spring6.service.UserService;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.util.Date;
+
 public class SpringDITest {
+
+    @Test
+    public void testMyDataSource() {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("set-di.xml");
+        MyDataSource myDataSource = applicationContext.getBean("myDataSource", MyDataSource.class);
+        System.out.println(myDataSource);
+    }
+
+    @Test
+    public void testSimpleType(){
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("set-di.xml");
+        /*User user = applicationContext.getBean("userBean", User.class);
+        System.out.println(user);*/
+
+        SimpleValueType svt = applicationContext.getBean("svt",SimpleValueType.class);
+        System.out.println(svt);
+    }
+
+    @Test
+    public void testSetDI2() {
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("set-di.xml");
+        OrderService orderService = applicationContext.getBean("orderServiceBean", OrderService.class);
+        orderService.generate();
+    }
 
     @Test
     public void testSetDI() {
