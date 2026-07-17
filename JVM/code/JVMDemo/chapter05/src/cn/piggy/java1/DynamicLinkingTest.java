0c39d8f4a6b07c0472c361c74344a41616f8c906
skyLake_vose <912214336@qq.com>
2026-07-17 17:05:56 +0800

    update

---
+++ JVM/code/JVMDemo/chapter05/src/cn/piggy/java1/DynamicLinkingTest.java
@@ -0,0 +1,14 @@
+package cn.piggy.java1;
+
+public class DynamicLinkingTest {
+    int num = 10;
+    public void methodA() {
+        System.out.println("methodA()...");
+    }
+
+    public void methodB() {
+        System.out.println("methodB()...");
+        methodA();
+        num++;
+    }
+}
