c69e58e620518d48fce06abe19c704890914ee3b
skyLake_vose <912214336@qq.com>
2026-07-27 11:43:47 +0800

    update

---
+++ JVM/code/JVMDemo/chapter05/src/cn/piggy/java2/Lambda.java
@@ -0,0 +1,23 @@
+package cn.piggy.java2;
+
+interface Func {
+    public boolean func(String str);
+}
+
+public class Lambda {
+    public void lambda(Func func) {
+        return;
+    }
+
+    public static void main(String[] args) {
+        Lambda lambda = new Lambda();
+        Func func = s -> {
+            return true;
+        };
+        lambda.lambda(func);
+
+        lambda.lambda(s -> {
+            return true;
+        });
+    }
+}
