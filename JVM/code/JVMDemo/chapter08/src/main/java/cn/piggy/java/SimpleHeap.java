d6da7271d04de7fb19e8193b5bc03983e706a580
skyLake_vose <912214336@qq.com>
2026-08-03 11:47:15 +0800

    update

---
+++ JVM/code/JVMDemo/chapter08/src/main/java/cn/piggy/java/SimpleHeap.java
@@ -0,0 +1,17 @@
+package cn.piggy.java;
+
+public class SimpleHeap {
+    private int id;
+
+    public SimpleHeap(int id) {
+        this.id = id;
+    }
+
+    public static void main(String[] args) {
+        SimpleHeap simpleHeap1 = new SimpleHeap(1);
+        SimpleHeap simpleHeap2 = new SimpleHeap(2);
+
+        int[] arr = new int[10];
+        Object[] arr1 = new Object[10];;
+    }
+}
